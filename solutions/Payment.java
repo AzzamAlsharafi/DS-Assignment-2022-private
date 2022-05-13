8aa3ccd1060c7fa69d6bbeff2f28322a94fd5d5f
Azzam Alsharafi <azzamalsharafi@gmail.com>
2022-05-14 01:57:34 +0800

    Add Payment.java

---
+++ solutions/Payment.java
@@ -0,0 +1,82 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Payment {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        PriorityQueue<txnId> queue = new PriorityQueue<>();
+
+        String[] firstLine = scanner.nextLine().split(" ");
+        long firstTime = Long.parseLong(firstLine[0]);
+
+        long firstStartTime = firstTime - startingTimeFromTier(firstLine[2].charAt(0));
+
+        queue.offer(new txnId(firstLine[1], firstStartTime));
+
+        long previousTime = firstTime;
+
+        while (true){
+            String[] line = scanner.nextLine().split(" ");
+
+            if(line.length == 3){
+                long time = Long.parseLong(line[0]);
+
+                long startTime = time - startingTimeFromTier(line[2].charAt(0));
+
+                queue.offer(new txnId(line[1], startTime));
+
+                if((time / 1000) - (previousTime / 1000) > 0){
+                    for (int i = 0; i < 100; i++) {
+                        System.out.print(queue.poll() + " ");
+                    }
+                    System.out.println();
+                }
+
+                previousTime = time;
+            } else {
+                if(line[0].equals("EXIT")){
+                    return;
+                } else {
+                    queue.clear();
+                }
+            }
+        }
+    }
+
+    public static long startingTimeFromTier(char firstChar){
+        return switch (firstChar) {
+            case 'B' -> // Bronze
+                    0;
+            case 'S' -> // Silver
+                    1000;
+            case 'G' -> // Gold
+                    2000;
+            case 'P' -> // Platinum
+                    3000;
+            default -> -1; // Won't happen.
+        };
+    }
+
+}
+
+class txnId implements Comparable<txnId>{
+    String id;
+    long time;
+
+    public txnId(String id, long time) {
+        this.id = id;
+        this.time = time;
+    }
+
+    @Override
+    public int compareTo(txnId o) {
+        return Long.compare(time, o.time);
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+}
